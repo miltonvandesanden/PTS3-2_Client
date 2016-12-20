37e768922c4b26d718f014eec2dcd75b05ea0f94
Soufyanbreda <Soufyanbargach@MacBook-Pro-van-Soufyan.local>
2016-12-20 13:43:39 +0100

    altered projectile class

--- S32T-PTS3/core/src/utils2/Projectile.java
+++ S32T-PTS3/core/src/utils2/Projectile.java
@@ -10,18 +10,20 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Rectangle;
 import java.io.Serializable;
 
 /**
  *
- * @author Danny
+ * @author Danny en souffy
  */
 public class Projectile implements Serializable{
     
         private float x, y, speed;
 	private boolean remove;
-        private Texture texture;
-        private Sprite sprite;
+       // private Texture texture;
+        //private Sprite sprite;
+         private Rectangle rectangle;
 	private PlayerCar car;
         private boolean firstLocation = true;
         float dirX;
@@ -33,12 +35,14 @@ public class Projectile implements Serializable{
                 car = c;
 		speed = 700;
 		remove = false;
-                if(texture == null)
-                {
-                    texture = new Texture("images/bullet.png");
-                    sprite = new Sprite(texture);
-                    sprite.setOrigin(x, y);
-                }
+                rectangle = new Rectangle(startX,startY,10f,10f);
+                
+//                if(texture == null)
+//                {
+//                    texture = new Texture("images/bullet.png");
+//                    sprite = new Sprite(texture);
+//                    sprite.setOrigin(x, y);
+//                }
 	}
 	
 	public void update(float deltaTime) {
@@ -55,7 +59,7 @@ public class Projectile implements Serializable{
         }
     }
         
-        public void render(SpriteBatch batch)
+        public void render(SpriteBatch batch, Sprite sprite)
         {
             batch.draw(sprite, x, y);
             //sprite.draw(batch);
@@ -94,3 +98,4 @@ public class Projectile implements Serializable{
 		this.remove = remove;
 	}
 }
+    
